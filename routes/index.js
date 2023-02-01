88b3120c455ec59b169d23d4a2c0d4d474d60b71
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-02-01 17:57:05 -0500

    iteration3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
+const punkAPI = new PunkAPIWrapper()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.hbs', { title: 'Express' });
 });
+
+router.get('/beers', ((req, res, next) =>{
+    punkAPI.getBeers().then()
+}))
 
 module.exports = router;
