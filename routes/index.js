be7bc910052eb0a3b2a5d9ea2917eb39217d0b08
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-02-02 10:35:50 -0500

    required done

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const PunkAPIWrapper = require('punkapi-javascript-wrapper')
-const punkAPI = new PunkAPIWrapper()
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
+const punkAPI = new PunkAPIWrapper();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.hbs', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index.hbs",);
 });
 
-router.get('/beers', ((req, res, next) =>{
-    punkAPI.getBeers().then()
+router.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromAPI) =>
+      res.render('beers.hbs', {beersFromAPI})
+    )
+    .catch((error) => console.log(error));
+});
+
+router.get('/random-beer', ((req, res, next)=>{
+  punkAPI
+  .getRandom()
+  .then((randomBeer) => {
+      // console.log(random)
+      res.render('random-beer.hbs', {randomBeer});
+  })
+  .catch(error => console.log(error));
+
 }))
 
 module.exports = router;
